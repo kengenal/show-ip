f84c67ebde10c44886db8bc3759c4a2d54260e8d
kengenal <kengenal1@gmail.com>
2021-05-22 13:56:23 +0200

    Add static options

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from main.settings import base
 
 urlpatterns = i18n_patterns(
     path("", include("show_ip.urls"))
-)
+) + static(base.STATIC_URL, document_root=base.STATIC_ROOT)
