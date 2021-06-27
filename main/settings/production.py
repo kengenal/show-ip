9d052064d5566053fdd73d21d6e08e7f889b7a72
kengenal <kengenal1@gmail.com>
2021-06-27 21:20:27 +0200

    Add static files and django confing for production

--- main/settings/production.py
+++ main/settings/production.py
@@ -1,13 +1,14 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["kengenal.pythonanywhere.com", "localhost"]
+ALLOWED_HOSTS = [os.getenv("SERVER_HOSTNAME", "localhost")]
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
-SECURE_HSTS_SECONDS = 60
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+if os.getenv("USE_SSL"):
+    SESSION_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 60
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
